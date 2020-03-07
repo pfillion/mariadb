61623bfa17ba7cd25b89d2d4149e52ab72735d0d
Pascal Fillion <pfillion@gmail.com>
2020-03-07 12:58:02 -0500

    adaption for new mobycron version

--- rootfs/usr/local/bin/entrypoint.sh
+++ rootfs/usr/local/bin/entrypoint.sh
@@ -3,7 +3,7 @@ set -eo pipefail
 shopt -s nullglob
 
 if [[ "${MOBYCRON_ENABLED}" == "true" || "${MOBYCRON_ENABLED}" == "1" ]]; then
-    mobycron &
+    mobycron -f /configs/config.json &
 fi
 
 if [ "$1" = 'mysqld' ]; then
